f6e7dd085a8a2a5933b15ad2e94ed8c228a386e1
Nasereddine Lakhal <naserlakhal@gmail.com>
2024-11-07 22:19:06 +0100

    task backend crp

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,7 @@ import morgan from "morgan";
 
 /* ROUTE IMPORTS */
 import projectRoutes from "./routes/projectRoutes";
+import taskRoutes from "./routes/taskRoutes";
 
 
 /* CONFIGURATIONS */
@@ -27,6 +28,7 @@ app.get("/", (req, res) => {
 });
   
 app.use("/projects", projectRoutes);
+app.use("/tasks", taskRoutes);
 
 
 /* SERVER */
